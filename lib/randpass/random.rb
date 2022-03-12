5797b35c7e212372076a798626a4f725a4a256a5
alx3dev <alx3dev@gmail.com>
2022-03-12 21:48:33 +0100

    fix - wrong number of characters

--- lib/randpass/random.rb
+++ lib/randpass/random.rb
@@ -6,29 +6,33 @@ require 'securerandom'
 # random special characters. Method #randpass is defined as
 # class and instance method, so you can call it or include it.
 #
-# @note Always shuffle #base64 if it has less than 16 chars, because they end with '=='
-#
 module Randpass
-  # allowed specials. xxx is flag for #add_special_chars
+  # Allowed specials. xxx is just a flag for #add_special_chars
   ARRAY = %w[! # * $ % _ @ xxx].freeze
 
   # Random number of times try to add random special character.
-  # Transform to array, shuffle, and join back to string
+  # Transform to array, shuffle, and join back to string.
   #
-  # @param [Integer] number_of_chars **Required**. Number of password characters.
+  # Provide number of characters for password as argument,
+  # default value is **22**.
+  #
+  # @param [Integer] number_of_chars Optional. Number of password characters.
   # @return [String]
   #
-  def randpass(number_of_chars)
+  def randpass(number_of_chars = nil)
     Randpass[number_of_chars]
   end
 
   class << self
     # @return [String]
-    def randpass(number_of_chars)
-      param = SecureRandom.base64(number_of_chars)
-      rand(5..10).times do
-        param = add_special_chars(param)
+    def randpass(number_of_chars = 20)
+      # slice string made with base64, to number of characters we want
+      param = SecureRandom.base64(number_of_chars)[0...number_of_chars]
+
+      rand(param.size / 2).times do
+        param = add_special_characters(param)
       end
+
       param.split('').shuffle.join
     end
 
@@ -36,9 +40,10 @@ module Randpass
 
     private
 
-    def add_special_chars(param)
-      char  = ARRAY.shuffle[rand(ARRAY.size)]
+    def add_special_characters(param)
+      char = ARRAY.shuffle[rand(ARRAY.size)]
       return param if char == 'xxx'
+
       param[rand(param.size)] = char.to_s
       param
     end
