7dc25934c4e034ce62036fc533ca8c970af48dee
alx3dev <alx3dev@gmail.com>
2022-12-15 17:08:52 +0100

    write comments

--- lib/randpass/storage.rb
+++ lib/randpass/storage.rb
@@ -3,9 +3,6 @@
 require 'tempfile'
 require 'fileutils'
 
-#
-# Under Development !!!
-#
 module Randpass
 
   class << self
@@ -14,19 +11,10 @@ module Randpass
 
     FILE_PATH = File.expand_path(__FILE__).gsub('/lib/randpass/storage.rb', '')
 
-    def initialized?; @initialized == true end
-
-
-    def storage_init
-      @path ||= FILE_PATH
-      Dir.chdir @path
-      @temp = String.new
-      @initialized = true
-    end
-
-    alias :init_storage :storage_init
-
-
+    # Add password to file
+    # @param [String] pass **Required**. Add password to list
+    # @param [String] comm Optional. Comment to write with password
+    #
     def add(pass, comm = nil)
       storage_init unless @initialized
       @temp << "#{comm}: " unless comm.nil?
@@ -34,14 +22,21 @@ module Randpass
       @temp << "\n"
     end
     
-
+    # Write list as .txt file
     def finalize
       File.write "#{@path}/randpass_#{Time.now.to_i}.txt", @temp
       @initialized = false
     end
 
-
-    def generate_list(chars_no = nil, opts = {list: 10})
+    # Generate list of passwords, and save as plain txt.
+    #
+    # @param [Integer] chars_no **Required**. Number of password characters.
+    # @param [Hash] opts Optional. Options hash.
+    #
+    # @option opts [String] comment Add comment (name) with password.
+    # @option opts [Integer] list Number of passwords to generate (if no comments)
+    #
+    def generate_list(chars_no, opts = {list: 10})
       storage_init unless initialized?
 
       if opts[:comments].nil?
@@ -57,9 +52,23 @@ module Randpass
       Randpass.finalize
     end
 
-    
+    # Access password list
     def storage
       @temp
     end
+
+
+    private
+
+    def initialized?; @initialized == true end
+
+    def storage_init
+      @path ||= FILE_PATH
+      Dir.chdir @path
+      @temp = String.new
+      @initialized = true
+    end
+
+    alias :init_storage :storage_init
   end
 end
